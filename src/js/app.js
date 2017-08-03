1052bd7a6e9836272e0ad748a886b1c347c7f1c2
Naoki Sawada <naoki.sawada@nextremer.com>
2017-08-03 23:32:40 +0900

    Update button area

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import Main from './components/Main';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { initial } from './reducers';
-import { MuiThemeProvider } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
+const theme = createMuiTheme();
 
 ReactDOM.render(
   <Provider store={configureStore(initial)}>
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
       <Main />
     </MuiThemeProvider>
   </Provider>,
--- src/js/components/ButtonArea.js
+++ src/js/components/ButtonArea.js
@@ -1,38 +1,70 @@
-import styles from './ButtonArea.css';
-import Button from 'material-ui/Button';
-import { LightbulbOutline, Mic, Camera, Videocam } from 'material-ui-icons';
 import { connect } from 'react-redux';
 import { buttonState } from 'actions';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import { LightbulbOutline, Mic, Camera, Videocam } from 'material-ui-icons';
+import styles from './ButtonArea.css';
 
-@connect(null, {
+const styleSheet = createStyleSheet(theme => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+  },
+  appBar: {
+    backgroundColor: '#E3314D',
+    color: '#fff',
+  },
+}));
+
+const buttonType = [
+  'light',
+  'mic',
+  'camera',
+  'video',
+];
+
+@connect(state => ({
+  button: state.button.state,
+}), {
   buttonState,
 })
-@CSSModules(styles)
+@withStyles(styleSheet)
 export default class ButtonArea extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { index: 0 };
     this.onTouchEvent = this.onTouchEvent.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   onTouchEvent(event, type) {
     this.props.buttonState(type);
   }
 
+  handleChange(event, index) {
+    this.setState({ index });
+    this.props.buttonState(buttonType[index]);
+  };
+
   render() {
+    const classes = this.props.classes;
     return (
-      <div styleName="buttonArea">
-        <Button onClick={e => this.onTouchEvent(e, 'light')}>
-          <LightbulbOutline />
-        </Button>
-        <Button onClick={e => this.onTouchEvent(e, 'mic')}>
-          <Mic />
-        </Button>
-        <Button onClick={e => this.onTouchEvent(e, 'camera')}>
-          <Camera />
-        </Button>
-        <Button onClick={e => this.onTouchEvent(e, 'video')}>
-          <Videocam />
-        </Button>
+      <div className={classes.root}>
+        <AppBar className={classes.appBar} position="static" color="default">
+          <Tabs
+            index={this.state.index}
+            onChange={this.handleChange}
+            fullWidth
+            centered
+            indicatorColor="#fff"
+          >
+            <Tab label="Light" icon={<LightbulbOutline />} />
+            <Tab label="Mic or Text input" icon={<Mic />} />
+            <Tab label="Camera" icon={<Camera />} />
+            <Tab label="Video" icon={<Videocam />} />
+          </Tabs>
+        </AppBar>
       </div>
     );
   }
